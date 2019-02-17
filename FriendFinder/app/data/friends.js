f5b0197782ac262a694568951020b38e14151a71
ukgt <ukgt2019@gmail.com>
2019-02-16 19:44:54 -0500

    html files and initial js file

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,28 @@
+// Holds the array of friends
+var friends = [{
+     "name": "Mallory Malone",
+     "photo": "https://www.pexels.com/photo/woman-wears-black-suit-jacket-photography-818819/",
+     "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+}, {
+     "name": "Jason Denny",
+     "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+     "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+}, {
+     "name": "Nandini Singh",
+     "photo": "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+     "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+}, {
+     "name": "Luke Jacobs",
+     "photo": "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+     "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+}, {
+     "name": "Lou Ritter",
+     "photo": "https://www.pexels.com/photo/accomplishment-action-adult-adventure-372098/",
+     "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+}, {
+     "name": "Jordan Brown",
+     "photo": "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+}];
+
+// Export the data
+module.exports = friends;
--- server.js
+++ server.js
@@ -3,3 +3,31 @@
 var express = require("express");
 var path = require("path");
 var inquirer = require("inquirer");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyParser.text({
+     type: "text/html"
+}));
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json);
+
+
+
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+     console.log("App listening on PORT " + PORT);
+});
